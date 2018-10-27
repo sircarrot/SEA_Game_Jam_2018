3d71c79cd52c2dc631500a54c912dea9b1208148
Royce Santo <roycesanto@yahoo.com>
2018-10-27 15:28:52 +0800

    Player UI and Animation

---
+++ Assets/Scripts/TestMusic.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMusic : MonoBehaviour {
+
+    public AudioClip click;
+    public AudioClip bgm;
+
+    public float baseTimer = 1f;
+    public float timer = 0;
+
+    public UIManager UIManager;
+    private AudioManager audioManager = null;
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (audioManager == null)
+        {
+            audioManager = Toolbox.Instance.GetManager<AudioManager>();
+
+            audioManager.BGMPlayer(bgm);
+        }
+
+        if(timer >= baseTimer)
+        {
+            audioManager.PlaySoundEffect(click);
+            timer -= baseTimer;
+
+            UIManager.TestFunction();
+        }
+
+        timer += Time.deltaTime;
+
+    }
+}
