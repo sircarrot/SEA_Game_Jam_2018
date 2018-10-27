3d27321cbc21ec6c27ff8e23636bc34bbc182451
Royce Santo <roycesanto@yahoo.com>
2018-10-27 12:39:48 +0800

    Input Manager simple logic

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private bool initComplete;
+
+    public void Init()
+    {
+        initComplete = true;
+    }
+
+    private void Update()
+    {
+        if (!initComplete) return;
+        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            // Healer, player 1
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // Healer, player 2
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // Free, player 1
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            // Free, player 2
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            // Harvester, player 1
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // Harvester, player 2
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            // Striker, player 1
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            // Striker, player 2
+        }
+    }
 }
