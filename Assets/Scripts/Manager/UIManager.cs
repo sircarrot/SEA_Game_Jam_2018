472b07ff77700fac8bbe6ce356e3e7d201eee05f
Royce Santo <roycesanto@yahoo.com>
2018-10-27 18:53:36 +0800

    UIManager update

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
-    public List<PlayerUI> playerUIList = new List<PlayerUI>();
+    private Text centerText;
+    private List<PlayerUI> playerUIList = new List<PlayerUI>();
 
-
+    public void UpdateText(int playerNumber, UnitTypes unitType, int number, int injured = 0)
+    {
+        playerUIList[playerNumber].UpdateText(unitType, number, injured);
+    }
 }
