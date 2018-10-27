23d00e88abc20220b2420740db84bc554d38b6c4
Royce Santo <roycesanto@yahoo.com>
2018-10-27 18:55:04 +0800

    New Center Text for headers/headlines

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -8,6 +8,11 @@ public class UIManager : MonoBehaviour {
     private Text centerText;
     private List<PlayerUI> playerUIList = new List<PlayerUI>();
 
+    public void CenterTextUpdate(string value)
+    {
+        centerText.text = value;
+    }
+
     public void UpdateText(int playerNumber, UnitTypes unitType, int number, int injured = 0)
     {
         playerUIList[playerNumber].UpdateText(unitType, number, injured);
