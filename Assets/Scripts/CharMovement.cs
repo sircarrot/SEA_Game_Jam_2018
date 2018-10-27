7e28527abf6cb5f9575d761ef1fd3b896116b8e0
fuadibr <ahmadfuad.ibrahim@gmail.com>
2018-10-27 15:40:20 +0800

    Character movement attack

--- Assets/Scripts/CharMovement.cs
+++ Assets/Scripts/CharMovement.cs
@@ -1,17 +1,119 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CharMovement : MonoBehaviour
 {
-    enum Jobs { Free, Attack, Heal, Harvest};
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    enum Jobs { Free, Attack, Heal, Harvest };
+    Jobs currentJob = Jobs.Attack;
+    public GameObject enemy;
+    private GameObject[] enemyArray;
+    NavMeshAgent agent;
+    enum AttackStage { chase, attack, cooldown};
+    AttackStage attckStage = AttackStage.chase;
+
+    int hp = 100;
+    int attackDamage = 10;
+    float attackCooldown = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentJob == Jobs.Free)
+        {
+            freeMode();
+        }
+        else if (currentJob == Jobs.Attack)
+        {
+            attackMode();
+        }
+        else if (currentJob == Jobs.Heal)
+        {
+            healMode();
+        }
+        else if (currentJob == Jobs.Harvest)
+        {
+            harvestMode();
+        }
+    }
+    
+    void freeMode()
+    {
+
+    }
+
+    void attackMode()
+    {
+        if (enemyArray == null)
+        {
+            enemyArray = GameObject.FindGameObjectsWithTag("Dog");
+        }
+        float closestDistance = 100000000;
+        foreach (GameObject dog in enemyArray)
+        {
+            Vector3 currentPosition = transform.position;
+            float distToTarget = Vector3.Distance(dog.transform.position,currentPosition);
+            if (distToTarget < closestDistance)
+            {
+                closestDistance = distToTarget;
+                enemy = dog;
+            }
+        }
+        if (closestDistance < 3.0f)
+        {
+            if (attackCooldown > 0)
+            {
+                attckStage = AttackStage.cooldown;
+                attackCooldown -= Time.deltaTime * 1.0f;
+            }
+            else
+            {
+                attckStage = AttackStage.attack;
+                attackCooldown = 5.0f;
+            }
+        }
+        else
+        {
+            attckStage = AttackStage.chase;
+        }
+        //Debug.Log(closestDistance);
+        if (attckStage == AttackStage.chase)
+        {
+            if (enemy != null)
+            {
+                agent.isStopped = false;
+                agent.destination = enemy.transform.position;
+            }
+        }
+        else if (attckStage == AttackStage.attack)
+        {
+            if (enemy != null)
+            {
+                agent.isStopped = true;
+                enemy.GetComponent<DogMove>().hurt();//inflict damage
+            }
+        }
+        else if (attckStage == AttackStage.cooldown)
+        {
+            agent.isStopped = true;
+        }
+    }
+
+    void healMode()
+    {
+
+    }
+
+    void harvestMode()
+    {
+
+    }
 }
