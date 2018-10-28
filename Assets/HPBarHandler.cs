1071166ef45b795a16e154e825c36c1e47e6a47e
Royce Santo <roycesanto@yahoo.com>
2018-10-28 11:37:28 +0800

    HP bar visibility

--- Assets/HPBarHandler.cs
+++ Assets/HPBarHandler.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HPBarHandler : MonoBehaviour {
-
+public class HPBarHandler : MonoBehaviour
+{
+    public Slider slider;
     public Camera camera;
-    public Transform referenceUnit;    
+    public Transform referenceUnit;
+    public GameObject visibility;
     public bool initComplete = false;
 
     public void Init(Transform reference, Camera cam)
     {
+        if (slider == null) slider = GetComponent<Slider>();
         referenceUnit = reference;
         camera = cam;
         initComplete = true;
@@ -25,6 +29,21 @@ public class HPBarHandler : MonoBehaviour {
             gameObject.transform.position = camera.WorldToScreenPoint(referenceUnit.position);
             gameObject.transform.position += new Vector3(0, 35, 0);
         }
-
     }
+
+    public void Damage(float percentage)
+    {
+        slider.value = percentage;
+        visibility.SetActive(true);
+    }
+
+    public void Heal(float percentage)
+    {
+        slider.value = percentage;
+        if(percentage >= 1f)
+        {
+            visibility.SetActive(false);
+        }
+    }
+
 }
