62582041a97eccf680a90ee6f2a42bd792d42809
Royce Santo <roycesanto@yahoo.com>
2018-10-28 13:40:10 +0800

    Captions

---
+++ Assets/Scripts/CaptionScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CaptionScript : MonoBehaviour {
+
+    public Text dialogueBox;
+
+    public InitText(string caption)
+    {
+        dialogueBox.text = caption
+    }
+
+}
--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
+
+    public GameObject speechBubble;
+    public GameObject movingCanvas;
+
     [SerializeField] private List<PlayerUI> playerUIList = new List<PlayerUI>();
 
     [SerializeField] private List<Text> playerPoints = new List<Text>();
@@ -22,5 +26,26 @@ public class UIManager : MonoBehaviour {
     public void UpdateText(int playerNumber, UnitTypes unitType, int number, int injured = 0)
     {
         playerUIList[playerNumber].UpdateText(unitType, number, injured);
+    }
+
+    public void CreateCaption()
+    {
+        GameObject bubble = Instantiate(speechBubble, movingCanvas.transform);
+
+        StartCoroutine(DestroyBubble(bubble));
+    }
+
+    private IEnumerator DestroyBubble(GameObject bubble)
+    {
+        float duration = 1;
+        while(duration > 0)
+        {
+            yield return null;
+            duration -= Time.deltaTime;
+        }
+
+        Destroy(bubble);
+
+
     }
 }
