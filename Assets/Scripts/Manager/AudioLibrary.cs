f953e1c40120fe4ffed2ee480180b0f2b989e0d6
Royce Santo <roycesanto@yahoo.com>
2018-10-28 09:46:55 +0800

    Audio Library

---
+++ Assets/Scripts/Manager/AudioLibrary.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLibrary : MonoBehaviour {
+
+    public AudioClip striker;
+    public AudioClip healer;
+    public AudioClip producer;
+
+    
+
+}
